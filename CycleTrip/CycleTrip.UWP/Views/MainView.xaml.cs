e350f1d9b75ce5f2991c59ce57b7ca02557886a4
kurt ziegler <klzig@live.com>
2017-11-28 01:15:28 -0700

    Upgraded to mvvmcross v5.5.

--- CycleTrip/CycleTrip.UWP/Views/MainView.xaml.cs
+++ CycleTrip/CycleTrip.UWP/Views/MainView.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using MvvmCross.Uwp.Views;
+using MvvmCross.Uwp.Attributes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -21,6 +22,7 @@ namespace CycleTrip.UWP.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    [MvxPagePresentation]
     public sealed partial class MainView : MvxWindowsPage
     {
         public MainView()
