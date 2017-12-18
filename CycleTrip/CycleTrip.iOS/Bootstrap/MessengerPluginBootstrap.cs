0e146578f8b103873c30efa5362897edd7f2ca8f
kurt ziegler <klzig@live.com>
2017-12-17 23:24:47 -0700

    Upgraded mvx to v5.6.2, including adding mvx messenger plugin to all platforms.  This fixed a build problem with UWP.

---
+++ CycleTrip/CycleTrip.iOS/Bootstrap/MessengerPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace CycleTrip.iOS.Bootstrap
+{
+    public class MessengerPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Messenger.PluginLoader>
+    {
+    }
+}
