5c40f091c7931d6141aacc75a0477e95ccb1405f
kurt ziegler <klzig@live.com>
2018-04-24 00:18:48 -0600

    Switched shared project from PCL to .NET Standard.

---
+++ CycleTrip/CycleTrip.Android/Bootstrap/ResxLocalizationPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace CycleTrip.Droid.Bootstrap
+{
+    public class ResxLocalizationPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.ResxLocalization.PluginLoader>
+    {
+    }
+}
---
+++ CycleTrip/CycleTrip.iOS/Bootstrap/ResxLocalizationPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace CycleTrip.iOS.Bootstrap
+{
+    public class ResxLocalizationPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.ResxLocalization.PluginLoader>
+    {
+    }
+}
