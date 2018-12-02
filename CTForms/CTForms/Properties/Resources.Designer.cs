067c6fa4e229fdbd66af0e1e42017043ced91f12
kurt ziegler <klzig@live.com>
2018-12-02 00:04:44 -0700

    Fixed hardcoded label text in menu header.

--- CTForms/CTForms/Properties/Resources.Designer.cs
+++ CTForms/CTForms/Properties/Resources.Designer.cs
@@ -61,6 +61,15 @@ namespace CTForms.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to CycleTrip.
+        /// </summary>
+        internal static string AppName {
+            get {
+                return ResourceManager.GetString("AppName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Add.
         /// </summary>
         internal static string LabelAdd {
